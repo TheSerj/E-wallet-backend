571a33bb08b119ddb5455c3a049dff29074b4151
devmittalciv16 <62499453+devmittalciv16@users.noreply.github.com>
2020-08-20 17:39:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res)=>{
     res.send("Hello");
 })
 app.use('/', authRouter);
-app.use('/dontAskWhy', express.static(path.join(__dirname, './public/')));
+
 const port = process.env.PORT||3001;
 app.listen(port, ()=>{
     console.log("App is running on 4000");
